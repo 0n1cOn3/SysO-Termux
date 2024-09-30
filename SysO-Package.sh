418b46e30ee10211ef080766837c270bf8b38c97
0n1cOn3 <0n1cOn3@gmx.ch>
2024-10-01 01:58:59 +0200

    Update to V2.0

--- SysO-Package.sh
+++ SysO-Package.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#
-# Created by: Informatic_in_Termux
-#
-# SysO-Package
-#
+# SysO-Tool - V2.0 - 2024
+# -----------------------
 # VARIABLES
-#
 source ${PWD}/Colors.sh
 source ${HOME}/SysO-Termux/config/Termux
 packages=(git,python,python2,wget,pvm,axel,openssh,php,vim,root-repo,unstable-repo,x11-repo,termux-api,clang,fish,ruby,w3m,figlet,cowsay,zip,wcalc,bmon,unrar,toilet,proot,golang,macchanger,httrack,wireless-tools,tree,mlocate,apache2,nodejs,neofetch,irssi,toilet)
